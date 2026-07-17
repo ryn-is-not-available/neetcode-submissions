8dd5edfde8c73a84471aceca1258b2a97bc870f9
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-07-17 18:17:03 +0100

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        int carc[26]={0};
+        for(int i=0;i<s.size();i++){
+            carc[s[i]-'a']++;
+            carc[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(carc[i]!=0)return false;
+        }
+        return true;
+    }
+};
