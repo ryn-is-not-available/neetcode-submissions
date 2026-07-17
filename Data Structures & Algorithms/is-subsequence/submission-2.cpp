58d475cf863ce78af72be8cbc9351400f1e91f72
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-07-17 18:56:35 +0100

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int s_ptr = 0, t_ptr = 0;
+        int m = s.size(), n = t.size();
+        while (s_ptr < m && t_ptr < n) {
+            if (s[s_ptr] == t[t_ptr]) {
+                s_ptr++;
+            }
+            t_ptr++;
+        }
+        return s_ptr == m;;
+    }
+};
