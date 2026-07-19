d0be21250ffe3236a03f1da3dfce10c4d0c155cf
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-07-19 14:15:51 +0100

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
+    int condidat=nums[0],count=1;
+    for(int i=1;i<nums.size();i++){
+        if(nums[i]==condidat)count++;
+        else count--;
+        if(count==0){
+            condidat=nums[i];
+            count++;
+            };
+    }
+    return condidat;
+    }
+};
