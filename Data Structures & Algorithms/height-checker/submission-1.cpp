75d93cdcfedf091d6c81be7bdd545e012806babd
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-07-21 17:45:19 +0100

    Add: height-checker - submission-1

---
+++ Data Structures & Algorithms/height-checker/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> count_map(101, 0);
+        for (int h : heights) {
+            count_map[h]++;
+        }
+
+        int mismatches = 0;
+        int expected_height = 1;
+
+        for (int actual_height : heights) {
+            while (count_map[expected_height] == 0) {
+                expected_height++;
+            }
+
+            if (actual_height != expected_height) {
+                mismatches++;
+            }
+
+            count_map[expected_height]--;
+        }
+
+        return mismatches;
+    }
+};
