6f55f966ab806c26decbea59d638c7c894e69812
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-07-22 20:40:09 +0100

    Add: pascals-triangle-ii - submission-1

---
+++ Data Structures & Algorithms/pascals-triangle-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        ios::sync_with_stdio(0);
+        cin.tie(NULL);
+        vector<int> row;
+        for(int i=0;i<=rowIndex;i++){
+            row.push_back(1);
+            for(int j=i-1;j>0;j--){
+                row[j]+=row[j-1];
+            }
+    
+        }
+        return row;
+
+
+    }
+};
