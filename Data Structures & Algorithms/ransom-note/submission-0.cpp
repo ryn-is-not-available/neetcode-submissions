033403abe843127b3e9f65a425cef1618bcda956
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-07-23 08:18:58 +0100

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int carac[26]={0};
+        if(ransomNote.size()>magazine.size())return false;
+
+        for(int i=0;i<magazine.size();i++){
+            carac[magazine[i]-'a']++;
+        }
+        for(int i=0;i<ransomNote.size();i++){
+            carac[ransomNote[i]-'a']--;
+            if(carac[ransomNote[i]-'a']<0)return false;
+        }
+        return true;
+    }
+};
