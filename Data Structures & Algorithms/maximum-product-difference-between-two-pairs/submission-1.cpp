87a93a49bc73fe6715cad5e227fdd9dd83229ca4
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-07-23 09:14:57 +0100

    Add: maximum-product-difference-between-two-pairs - submission-1

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+       int max1=-1,max2=-2,min1=10001,min2=10002;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]>max1){
+            max2=max1;
+            max1=nums[i];
+        }else if(nums[i]>max2){
+            max2=nums[i];
+        }
+        if(nums[i]<min1){
+            min2=min1;
+            min1=nums[i];
+        }else if(nums[i]<min2){
+            min2=nums[i];
+        }
+       } 
+       return max1*max2-min1*min2;
+    }
+};
