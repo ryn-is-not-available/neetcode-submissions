70c6321e737311df4b58464aa5ac17227ca1dd80
Massek Rayen <rayen.massek@insat.ucar.tn>
2026-09-06 17:27:34 +0100

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
+        int missing,left,right;
+        
+        for (int i=0;i<numbers.size();i++) {
+            missing = target - numbers[i];
+            left=i+1;
+            right = numbers.size()-1;
+            while(left<=right){
+                int mid = (left+right)/2;
+                if (numbers[mid]<missing) left = mid + 1;
+                else if (numbers[mid]>missing) right = mid - 1;
+                else {
+                    return {i+1,mid+1};
+                }
+            }
+        }
+        return {};  
+    }
+};
